34f20eec2b90c253566d8456ae348bd77c1f4a45
Santhosh J R <137148886+Santhu7760@users.noreply.github.com>
2025-06-11 21:14:37 +0530

    Add files via upload

---
+++ my projects/QR-Code generator/main.py
@@ -0,0 +1,14 @@
+import qrcode
+import qrcode.constants
+
+qr=qrcode.QRCode(
+    version=2,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=2
+)
+
+qr.add_data("hello di!")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="black")
+img.save("image.png")
